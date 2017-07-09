3eb819bdcc5abb2381cd1dc9d4ef3eb58f9a081a
Calvin Low <calvinlow@Calvins-MacBook-Air.local>
2017-07-09 15:16:53 +0800

    Add changes to main.cpp

--- CarND-PID-Control-Project/src/PID.cpp
+++ CarND-PID-Control-Project/src/PID.cpp
@@ -1,5 +1,4 @@
 #include "PID.h"
-#include <cmath>
 
 using namespace std;
 
@@ -12,19 +11,21 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
-	PID::Kp = Kp;
-    PID::Ki = Ki;
-    PID::Kd = Kd;
-	p_error = d_error = i_error = 0.0;
+  this->Kp = Kp;
+  this->Ki = Ki;
+  this->Kd = Kd;
+  p_error = 0;
+  d_error = 0;
+  i_error = 0;
 }
 
-void PID::UpdateError(double cte) {	
-	i_error += cte;
-	d_error = p_error - cte;
-	p_error = cte;
+void PID::UpdateError(double cte) {
+  i_error = cte + i_error;
+  d_error = cte - p_error;
+  p_error = cte;
 }
 
 double PID::TotalError() {
-	return -Kp * p_error - Kd * d_error - Ki * i_error;
+  return -Kp * p_error - Kd * d_error - Ki * i_error;
 }
 
